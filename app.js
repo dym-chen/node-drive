7994128f9d485de30db5df29b29fef3a5463ea27
dym-chen <danielymchen@gmail.com>
2025-07-05 20:34:34 -0400

    creating db with sqlite

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const express = require('express');
+import express from 'express';
+import db from './database.js'; 
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Hello', status: 'success' });
+    res.send('get request received');
+});
+
+app.post('/', (req, res) => {
+    res.send('post request received');
+    
 });
 
 app.listen(port, () => {
